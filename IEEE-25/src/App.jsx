2178b6746983ae6ea8f541ebf748ef7ebd31baae
حبيبة مصطفى محمود قناوى <143437900+Habibakenawy@users.noreply.github.com>
2025-07-15 00:08:15 +0300

    Added Flowbite

--- IEEE-25/src/App.jsx
+++ IEEE-25/src/App.jsx
@@ -1,11 +1,13 @@
 //import { useState } from 'react'
 import './App.css'
 import Home from './Components/Home'
+import { Button } from "flowbite-react";
 
 function App() {
   
   return (
     <>
+     <Button className="bg-red-500 hover:bg-red-600">Custom Button</Button>
     <Home/>
     </>
   )
