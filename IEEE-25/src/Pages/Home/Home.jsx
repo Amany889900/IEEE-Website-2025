aa1cd0f0089aa2503b0023866b362c9bca5298b8
حبيبة مصطفى محمود قناوى <143437900+Habibakenawy@users.noreply.github.com>
2026-03-02 21:18:29 +0200

    feat(home): Addition of the registration button

--- IEEE-25/src/Pages/Home/Home.jsx
+++ IEEE-25/src/Pages/Home/Home.jsx
@@ -1,42 +1,42 @@
-import React from 'react'
+import React from "react";
 
 import styles from "./Home.module.css"; // import styles object
 
-import bulb from "./../../assets/images/bulb.png"
+import bulb from "./../../assets/images/bulb.png";
 
 function Home() {
-
   return (
-<>
+    <>
+      <div className="w-[80%] mx-auto flex flex-col md:flex-row justify-center md:justify-between items-center h-screen">
+        {/* Text Section */}
+        <div className="w-full md:w-[60%] text-center md:text-left mb-10 md:mb-0">
+          <p className="text-white text-4xl sm:text-5xl md:text-6xl font-extrabold uppercase mb-6 drop-shadow-lg">
+            electrifying minds
+          </p>
+          <p
+            className={` text-4xl sm:text-5xl md:text-6xl  font-medium uppercase text-transparent ${styles.neonText}`}
+          >
+            illuminating future
+          </p>
+        </div>
 
- 
- 
-  <div className="w-[80%] mx-auto flex flex-col md:flex-row justify-center md:justify-between items-center h-screen">
-  {/* Text Section */}
-  <div className="w-full md:w-[60%] text-center md:text-left mb-10 md:mb-0">
-    <p className="text-white text-4xl sm:text-5xl md:text-6xl font-extrabold uppercase mb-6 drop-shadow-lg">
-      electrifying minds
-    </p>
-    <p className={` text-4xl sm:text-5xl md:text-6xl  font-medium uppercase text-transparent ${styles.neonText}`}>
-      illuminating future
-    </p>
-  </div>
-
-  {/* Bulb Section */}
-  <div className="w-2/3 sm:w-1/2 md:w-[40%] lg:w-[30%]">
-    <img
-      src={bulb}
-      alt="glowing bulb"
-      className={`w-full mx-auto ${styles.glowingBulb}`}
-    />
-  </div>
-</div>
-
-
-
-    
-</>
-  )
+        {/* Bulb Section */}
+        <div className="w-2/3 sm:w-1/2 md:w-[40%] lg:w-[30%]">
+          <img
+            src={bulb}
+            alt="glowing bulb"
+            className={`w-full mx-auto ${styles.glowingBulb}`}
+          />
+        </div>
+      </div>
+      {/* Registration Button */}
+      <div className="w-full flex justify-center items-center">
+        <button className="px-8 py-3 text-lg font-bold text-white uppercase transition-all duration-300 border-2 border-white rounded-full hover:bg-white hover:text-black hover:shadow-[0_0_20px_rgba(255,255,255,0.8)]">
+          Be a Team Member
+        </button>
+      </div>
+    </>
+  );
 }
 
-export default Home
+export default Home;
