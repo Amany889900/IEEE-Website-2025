6076dbf675d68023553ab3091bea68a1cfe38634
حبيبة مصطفى محمود قناوى <143437900+Habibakenawy@users.noreply.github.com>
2026-03-05 22:02:04 +0200

    Update Registration.jsx

--- IEEE-25/src/Pages/Registration/Registration.jsx
+++ IEEE-25/src/Pages/Registration/Registration.jsx
@@ -261,16 +261,13 @@ const Registration = () => {
     setErrors({});
   };
 
-  const hasErrors =
-    Object.values(errors).some((err) => err) ||
-    Object.entries(formData).some(
-      ([key, val]) =>
-        key !== "linkedInUrl" &&
-        key !== "secondPreference" &&
-        key !== "cv" &&
-        !val,
-    );
-
+const hasErrors =
+  Object.values(errors).some((err) => err) ||
+  Object.entries(formData).some(([key, val]) => {
+    const optionalFields = ["linkedInUrl", "secondPreference", "cv"];
+    if (optionalFields.includes(key)) return false;
+    return !val;
+  });
   return (
     <div className="min-h-screen py-12 px-4 selection:bg-cyan-500/30 ">
       {/* Confirmation Modal */}
